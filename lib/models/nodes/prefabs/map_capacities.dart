fc5099c354ebabb013d681601e146b963f1c9bb1
James Chen <james@ashchan.com>
2020-07-09 15:06:21 +0900

    Update balance prefab to use more helper methods

--- lib/models/nodes/prefabs/map_capacities.dart
+++ lib/models/nodes/prefabs/map_capacities.dart
@@ -1,9 +1,10 @@
+import 'package:fixnum/fixnum.dart';
+
 import 'helper.dart';
 
 import '../../nodes/ast_node.dart';
 import '../../nodes/prefab_node.dart';
 import '../../nodes/get_op_node.dart';
-import '../../nodes/primitive_node.dart';
 
 List<AstNode> convertMapCapacities(PrefabNode node) {
   final mapCapacities = AstNode(valueType: ValueType.map, position: node.position);
@@ -12,9 +13,8 @@ List<AstNode> convertMapCapacities(PrefabNode node) {
   final getCapacity = GetOpNode(valueType: ValueType.getCapacity);
   mapCapacities.addChild(getCapacity, mapCapacities.addSlot('get capacity').id);
 
-  final arg0 = PrimitiveNode(valueType: ValueType.arg);
+  final arg0 = arg(Int64(0));
   arg0.name = 'arg0';
-  arg0.value = '0';
   getCapacity.addChild(arg0, getCapacity.slots.first.id);
 
   final cells = PrefabNode(valueType: ValueType.prefabQueryCells);
--- lib/models/nodes/prefabs/query_cells.dart
+++ lib/models/nodes/prefabs/query_cells.dart
@@ -1,8 +1,9 @@
+import 'package:fixnum/fixnum.dart';
+
 import 'helper.dart';
 
 import '../../nodes/ast_node.dart';
 import '../../nodes/prefab_node.dart';
-import '../../nodes/op_node.dart';
 import '../../nodes/get_op_node.dart';
 import '../../nodes/primitive_node.dart';
 
@@ -13,44 +14,35 @@ List<AstNode> convertQueryCells(PrefabNode node) {
   final queryTest = AstNode(valueType: ValueType.and);
   queryCells.addChild(queryTest, queryCells.addSlot('and').id);
 
-  final codeHash = OperationNode(valueType: ValueType.equal);
+  final getCodeHash = GetOpNode(valueType: ValueType.getCodeHash);
+  final codeHashValue = PrimitiveNode(valueType: ValueType.bytes);
+  codeHashValue.value = secpTypeHash;
+  final codeHash = equal(getCodeHash, codeHashValue);
   codeHash.name = 'code hash';
   queryTest.addChild(codeHash, queryTest.addSlot('code hash').id);
 
-  final getCodeHash = GetOpNode(valueType: ValueType.getCodeHash);
-  codeHash.addChild(getCodeHash, codeHash.slots.first.id);
-  final codeHashValue = PrimitiveNode(valueType: ValueType.bytes);
-  codeHashValue.value = secpTypeHash;
-  codeHash.addChild(codeHashValue, codeHash.slots.last.id);
-
   final lock = GetOpNode(valueType: ValueType.getLock);
   lock.name = 'script lock';
-  final arg0 = PrimitiveNode(valueType: ValueType.arg);
+  final arg0 = arg(Int64(0));
   arg0.name = 'arg0 as cell';
-  arg0.value = '0';
   lock.addChild(arg0, lock.slots.first.id);
 
   getCodeHash.addChild(lock, getCodeHash.slots.first.id);
 
-  final hashType = OperationNode(valueType: ValueType.equal);
-  hashType.name = 'hash type';
-  queryTest.addChild(hashType, queryTest.addSlot('hash type').id);
   final getHashType = GetOpNode(valueType: ValueType.getHashType);
   getHashType.addChild(lock, getHashType.slots.first.id);
-  hashType.addChild(getHashType, hashType.slots.first.id);
-  final hashTypeValue = PrimitiveNode(valueType: ValueType.uint64);
-  hashTypeValue.value = '1';
-  hashType.addChild(hashTypeValue, hashType.slots.last.id);
+  final hashTypeValue = uintValue(Int64(1));
+  final hashType = equal(getHashType, hashTypeValue);
+  hashType.name = 'hash type';
+  queryTest.addChild(hashType, queryTest.addSlot('hash type').id);
 
-  final args = OperationNode(valueType: ValueType.equal);
-  args.name = 'args';
-  queryTest.addChild(args, queryTest.addSlot('args').id);
   final getArgs = GetOpNode(valueType: ValueType.getArgs);
   getArgs.addChild(lock, getArgs.slots.first.id);
-  args.addChild(getArgs, args.slots.first.id);
-  final param0 = PrimitiveNode(valueType: ValueType.param);
+  final param0 = param(Int64(0));
   param0.name = 'param0';
-  args.addChild(param0, args.slots.last.id);
+  final args = equal(getArgs, param0);
+  args.name = 'args';
+  queryTest.addChild(args, queryTest.addSlot('args').id);
 
   return [autoLayout(queryCells, initialPosition: node.position)];
 }
