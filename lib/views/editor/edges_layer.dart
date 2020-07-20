b9a6ad3412276b526e91772f88d2d68c0c4a1d45
James Chen <james@ashchan.com>
2020-07-20 15:58:41 +0900

    Do not render edges outside the visible area

--- lib/views/editor/edges_layer.dart
+++ lib/views/editor/edges_layer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'editor_dimensions.dart';
 import '../../models/document.dart';
 import '../../models/selection.dart';
 import '../../models/node.dart';
@@ -16,15 +17,38 @@ class EdgesLayer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer3<Document, Selection, EditorState>(
         builder: (context, document, selection, editorState, child) {
+      final links =
+          _visibleLinks(EditorDimensions.visibleCanvasArea(context).size, editorState, document);
       return CustomPaint(
         painter: _EdgesPainter(
-          document.links,
+          links,
           selection.selectedNode(document.nodes),
           editorState.canvasOffset,
         ),
         child: Container(),
       );
     });
+  }
+
+  List<Link> _visibleLinks(Size contextSize, EditorState state, Document doc) {
+    final visibleArea = Rect.fromLTWH(
+      -state.canvasOffset.dx,
+      -state.canvasOffset.dy,
+      contextSize.width / state.zoomScale,
+      contextSize.height / state.zoomScale,
+    );
+    return doc.links.where((link) {
+      return _isInsideVisibleArea(visibleArea, link);
+    }).toList();
+  }
+
+  bool _isInsideVisibleArea(Rect visibleArea, Link link) {
+    bool inside(Offset pos) => visibleArea.contains(pos);
+
+    return inside(link.parent.position) ||
+        inside(link.parent.size.bottomRight(link.parent.position)) ||
+        inside(link.child.position) ||
+        inside(link.child.size.bottomRight(link.child.position));
   }
 }
 
