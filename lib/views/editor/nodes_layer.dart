846640ae711e5cd22641ef9e6811433a8f4cd479
James Chen <james@ashchan.com>
2020-07-20 16:13:01 +0900

    Do not render nodes outside the visible area

--- lib/views/editor/nodes_layer.dart
+++ lib/views/editor/nodes_layer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'editor_dimensions.dart';
 import '../../models/document.dart';
 import '../../models/selection.dart';
+import '../../models/editor_state.dart';
 import '../../models/node.dart';
 
 import '../node.dart';
@@ -10,11 +12,20 @@ import '../node.dart';
 class NodesLayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer2<Document, Selection>(
-      builder: (context, document, selection, child) {
+    return Consumer3<Document, Selection, EditorState>(
+      builder: (context, document, selection, editorState, child) {
+        final contextSize = EditorDimensions.visibleCanvasArea(context).size;
+        final visibleArea = Rect.fromLTWH(
+          -editorState.canvasOffset.dx,
+          -editorState.canvasOffset.dy,
+          contextSize.width / editorState.zoomScale,
+          contextSize.height / editorState.zoomScale,
+        );
+        final nodes = _visibleNodes(visibleArea, document);
+
         return Stack(
           overflow: Overflow.visible,
-          children: document.nodes.map((node) {
+          children: nodes.map((node) {
             return ChangeNotifierProvider<Node>.value(
               value: node,
               child: ViewCreator.create(node),
@@ -23,5 +34,12 @@ class NodesLayer extends StatelessWidget {
         );
       },
     );
+  }
+
+  List<Node> _visibleNodes(Rect visibleArea, Document doc) {
+    return doc.nodes.where((node) {
+      return visibleArea.contains(node.position) ||
+          visibleArea.contains(node.size.bottomRight(node.position));
+    }).toList();
   }
 }
