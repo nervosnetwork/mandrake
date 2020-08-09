5ef4cb3006a4a88484dccb577148bfb7fd3fcd17
James Chen <james@ashchan.com>
2020-08-09 15:14:26 +0900

    Test main components of the editor

--- test/editor_test.dart
+++ test/editor_test.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mandrake/editor.dart';
+import 'package:mandrake/object_library.dart';
+import 'package:mandrake/property_inspector.dart';
+import 'package:mandrake/toolbar.dart';
+import 'package:mandrake/ruler.dart';
 
 void main() {
   final app = MaterialApp(
@@ -11,6 +15,18 @@ void main() {
   testWidgets('Editor shows template dialog on launch', (WidgetTester tester) async {
     await tester.pumpWidget(app);
     await tester.pumpAndSettle();
+
     expect(find.text('Choose a template'), findsOneWidget);
+  });
+
+  testWidgets('Main components', (WidgetTester tester) async {
+    await tester.pumpWidget(app);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(DesignEditor), findsOneWidget);
+    expect(find.byType(ObjectLibrary), findsOneWidget);
+    expect(find.byType(PropertyInspector), findsOneWidget);
+    expect(find.byType(Toolbar), findsOneWidget);
+    expect(find.byType(Ruler), findsNWidgets(2));
   });
 }
