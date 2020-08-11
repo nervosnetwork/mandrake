4e960d3f00a7e33d962f2279a8bf0634d490bc27
James Chen <james@ashchan.com>
2020-08-11 12:15:05 +0900

    Add tool/env script to generate/update lib/env file

---
+++ lib/env.dart
@@ -0,0 +1,6 @@
+/// Note: this will be overwritten when deploying. If there's any configuration that should not
+/// be overwritten, adjust tools/env.dart.
+final config = {
+  // Default dev client id, only used for local development.
+  'googleClientId': '386449909060-3bghhjo9fsfrfroenpgdd3d7jjl34fr1.apps.googleusercontent.com',
+};
---
+++ tool/env.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+Future<void> main() async {
+  final config = {
+    'googleClientId': Platform.environment['GOOGLE_CLIENT_ID'] ??
+        '386449909060-3bghhjo9fsfrfroenpgdd3d7jjl34fr1.apps.googleusercontent.com',
+  };
+
+  final filename = 'lib/env.dart';
+  await File(filename).writeAsString('final environment = ${json.encode(config)};');
+}
