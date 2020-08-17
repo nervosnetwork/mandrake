446c2e6f7c4622d0e95ba84fa70d2e382408e3f4
James Chen <james@ashchan.com>
2020-08-17 13:28:15 +0900

    Push a dummy initial change to undo stack when undo manager is initalized

--- lib/models/undo_manager.dart
+++ lib/models/undo_manager.dart
@@ -3,4 +3,15 @@ export 'package:undo/undo.dart' show Change;
 
 class UndoManager extends ChangeStack {
   static final UndoManager shared = UndoManager();
+
+  UndoManager() : super() {
+    /// The `undo` library we use assumes that there should be at least one history item
+    /// to enable undo. Add one dummy change to satisfy that.
+    /// https://github.com/rodydavis/undo/blob/master/lib/src/undo_stack.dart#L16-L17
+    add(Change(
+      0,
+      () {},
+      (_) {},
+    ));
+  }
 }
