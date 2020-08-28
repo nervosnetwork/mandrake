63098b2a561d3311abaeba76d224dddf9f1ad626
ashchan <james@ashchan.com>
2020-08-28 04:05:44 +0000

    Deploying to gh-pages from  @ 00efba253db8fe5d7584ca0fc3490305abc1b68a 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "5b2952b4fbebca21a4308a3e170f2e99",
 "/": "5b2952b4fbebca21a4308a3e170f2e99",
-"main.dart.js": "83a673f830412b293ac577e91a4c9541",
+"main.dart.js": "24f58d9616d6f87468fdd97bef29f09b",
 "favicon.png": "71d414da28ea51edf68426cd2a7cb7ba",
 "icons/Icon-192.png": "5c71b22049c5bdb0af970a194c69e0c0",
 "icons/Icon-512.png": "fa07c5171efe91402991ff53b2332b62",
